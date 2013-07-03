ac45fc03e7bbabd3bd00550fb31c8a7ec678fe1b
David da Silva <daviddasilvacontin@me.com>
2013-07-03 17:01:06 +0200

    Fixed messed up true/false

--- Keyboard.js
+++ Keyboard.js
@@ -4,14 +4,14 @@ function KeyboardJS (debug, preventer) {
     this.debug = debug;
     var scope = this;
     document.addEventListener("keydown", function (evt) {
-        scope.keysASCII[evt.keyCode] = false;
-        scope.keysCHAR[String.fromCharCode(evt.keyCode)] = false;
+        scope.keysASCII[evt.keyCode] = true;
+        scope.keysCHAR[String.fromCharCode(evt.keyCode)] = true;
         if (scope.debug) console.log('-- keyIsDown ASCII('+evt.keyCode+') CHAR('+String.fromCharCode(evt.keyCode)+')');
         if (preventer) preventer(evt);
     });
     document.addEventListener("keyup", function (evt) {
-        scope.keysASCII[evt.keyCode] = true;
-        scope.keysCHAR[String.fromCharCode(evt.keyCode)] = true;
+        scope.keysASCII[evt.keyCode] = false;
+        scope.keysCHAR[String.fromCharCode(evt.keyCode)] = false;
         if (scope.debug) console.log('-- keyIsUp ASCII('+evt.keyCode+') CHAR('+String.fromCharCode(evt.keyCode)+')');
         if (preventer) preventer(evt);
     });
