6df4f54d036c2b44da7b49b945fe764fe9fce561
David da Silva <daviddasilvacontin@me.com>
2013-07-03 11:18:55 +0200

    Initial commit

---
+++ Keyboard.js
@@ -0,0 +1,19 @@
+function KeyboardJS (debug, preventer) {
+    this.keysASCII = {};
+    this.keysCHAR = {};
+    this.debug = debug;
+    var scope = this;
+    document.addEventListener("keydown", function (evt) {
+        scope.keysASCII[evt.keyCode] = false;
+        scope.keysCHAR[String.fromCharCode(evt.keyCode)] = false;
+        if (scope.debug) console.log('-- keyIsDown ASCII('+evt.keyCode+') CHAR('+String.fromCharCode(evt.keyCode)+')');
+        if (preventer) preventer(evt);
+    });
+    document.addEventListener("keyup", function (evt) {
+        scope.keysASCII[evt.keyCode] = true;
+        scope.keysCHAR[String.fromCharCode(evt.keyCode)] = true;
+        if (scope.debug) console.log('-- keyIsUp ASCII('+evt.keyCode+') CHAR('+String.fromCharCode(evt.keyCode)+')');
+        if (preventer) preventer(evt);
+    });
+    if (scope.debug) console.log("keyboardJS inited", "keyboardJS");
+}
--- README.md
+++ README.md
@@ -2,3 +2,13 @@ KeyboardJS
 ==========
 
 Simple keyboard event handler.
+
+```javascript
+var debug = true;
+var preventer = function (evt) {
+	if (condition) evt.preventDefault();
+};
+var keyboard = new KeyboardJS(debug, preventer);
+
+if (keyboard.keysASCII['W']) moveForward();
+```
